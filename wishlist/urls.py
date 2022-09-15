85cd14141a630f6e6faa1993b2a80c7f4e4caca5
Aulia Satrio Wijoyo <aulia.satrio11@ui.ac.id>
2022-09-15 14:38:15 +0700

    add json by id

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from wishlist.views import show_wishlist
 from wishlist.views import show_barang
 from wishlist.views import show_json
+from wishlist.views import show_json_by_id
 
 app_name = 'wishlist'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_barang, name="show_barang"),
     path('json/', show_json, name="show_json"),
+    path('json/<int:id>', show_json_by_id, name="show_json_by_id"),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -18,3 +18,7 @@ def show_barang(request):
 def show_json(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
