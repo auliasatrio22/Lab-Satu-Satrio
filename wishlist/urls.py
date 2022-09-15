670f977e8938af8f02cddca1dbcdd2be5cc52e23
Aulia Satrio Wijoyo <aulia.satrio11@ui.ac.id>
2022-09-15 14:27:31 +0700

    Final Progres Lab 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_barang
+from wishlist.views import show_json
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_barang, name="show_barang"),
+    path('json/', show_json, name="show_json"),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
@@ -8,4 +10,11 @@ def show_wishlist(request):
         'nama': 'Aulia Satrio Wijoyo'
     }
     return render(request, "wishlist.html", context)
-    
+
+def show_barang(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
